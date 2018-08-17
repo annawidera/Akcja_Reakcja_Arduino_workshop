29e6b6e869ce4425560cb1770ad0007b71c6b13a
Anna Widera <anna.widera@thesoftwarehouse.pl>
2018-08-17 21:51:58 +0200

    Simplify APDS9960 example sketches
    Remove Callbacks.

--- GestureSensor/GestureSensor.ino
+++ GestureSensor/GestureSensor.ino
@@ -1,27 +1,6 @@
 #include <Adafruit_APDS9960.h>
-#include <Callback.h>
 
 Adafruit_APDS9960 apds;
-
-
-Signal<uint8_t> gestureDetectedSignal;
-
-
-void gestureDetectionHandler(uint8_t gesture) {
-  
-    if(gesture == APDS9960_DOWN) {
-      Serial.println("v");
-    } else if(gesture == APDS9960_UP) {
-      Serial.println("^");
-    } else if(gesture == APDS9960_LEFT) {
-      Serial.println("<");
-    } else if(gesture == APDS9960_RIGHT) {
-      Serial.println(">");
-    }
-}
-
-
-FunctionSlot<uint8_t> gestureDetectionSlot(gestureDetectionHandler); 
 
 
 void setup() {
@@ -38,13 +17,27 @@ void setup() {
   // for gesture mode, proximity is required
   apds.enableProximity(true);
   apds.enableGesture(true);
-  
-  gestureDetectedSignal.Connect(gestureDetectionSlot);
 }
 
 
 
 void loop() {
 
-  gestureDetectedSignal.Emit(apds.readGesture());
+  checkGesture();
 }
+
+
+void checkGesture() { 
+
+    uint8_t gesture = apds.readGesture();
+    if(gesture == APDS9960_DOWN) {
+      Serial.println("v");
+    } else if(gesture == APDS9960_UP) {
+      Serial.println("^");
+    } else if(gesture == APDS9960_LEFT) {
+      Serial.println("<");
+    } else if(gesture == APDS9960_RIGHT) {
+      Serial.println(">");
+    }
+}
+
--- ProximitySensor/ProximitySensor.ino
+++ ProximitySensor/ProximitySensor.ino
@@ -1,18 +1,6 @@
 #include <Adafruit_APDS9960.h>
-#include <Callback.h>
 
 Adafruit_APDS9960 apds;
-
-
-Signal<uint8_t> proximityMeasurementSignal; 
-
-
-void proximityMeasurementHandler(uint8_t proximity) {
-
-  Serial.println("proximity: "); Serial.println(proximity);
-}
-
-FunctionSlot<uint8_t> proximityMeasurementSlot(proximityMeasurementHandler); 
 
 
 void setup() {
@@ -29,13 +17,19 @@ void setup() {
   apds.setProxGain(APDS9960_PGAIN_8X);
   // for gesture mode, proximity is required
   apds.enableProximity(true);
-  
-  proximityMeasurementSignal.Connect(proximityMeasurementSlot);
 }
 
 
 
 void loop() {
 
-  proximityMeasurementSignal.Emit(apds.readProximity());
+  Serial.print("proximity: "); Serial.println(getProximity());
+  delay(300);  
 }
+
+
+uint8_t getProximity() { 
+
+  return apds.readProximity(); 
+}
+

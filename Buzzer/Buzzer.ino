6f605f23c50a0c5e3f43d0d9161097845d962ab1
Anna Widera <anna.widera@thesoftwarehouse.pl>
2019-03-15 19:28:16 +0100

    Update playing songs on Buzzer
    Change Songs definition, add new songs.

--- Buzzer/Buzzer.ino
+++ Buzzer/Buzzer.ino
@@ -1,63 +1,36 @@
+#include "Notes.h"
+#include "MarioSong.h"
+#include "PirattesSong.h"
 #define SPEAKER_PIN 9
 
-struct Song {
-  int length; 
-  char notes[25];
-  int beats[25];
-  int tempo; 
-};
-
-Song song1 = { 15, "ccggaagffeeddc ", { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 }, 300}; 
-Song song2 = { 20, "geefddceg geefddcec ", { 2, 2, 2, 2, 2, 2, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 1, 4, 4 }, 200 };
-Song song3 = { 18, "cdefgabC Cbagfedc ", { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4 }, 300};
+int note = 0;
+int songLength = sizeof(pirattes)/sizeof(Sound);
+float tempo = 1.5;
 
 
 void setup() {
+  
   pinMode(SPEAKER_PIN, OUTPUT);
+  Serial.begin(115200);
 }
 
-int note = 0;
-Song currentSong = song3;
 
 void loop() {
 
-  playNote(currentSong.notes[note], currentSong.beats[note] * currentSong.tempo);
-  delay(currentSong.tempo/2);
- 
+  playNote(pirattes[note].note, pirattes[note].beat * tempo);
+  
   note++; 
-
-  if (note >= currentSong.length) {
+  Serial.println(songLength);
+  if (note >= songLength) {
     note = 0; 
   }
 }
 
 
-// Those functions are  from here: https://www.arduino.cc/en/Tutorial/Melody
-
-void playNote(char note, int duration) {
+void playNote(int note, int duration) {
   
-  if (note == ' ') {
-    delay(duration); 
-    return;
-  }
-  
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
-
-  // play the tone corresponding to the note name
-  for (int i = 0; i < 8; i++) {
-    if (names[i] == note) {
-      playTone(tones[i], duration);
-    }
-  }
+  tone(SPEAKER_PIN, note, duration);  //tone(pin,frequency,duration)
+  delay(duration);
 }
 
-void playTone(int tone, int duration) {
-  
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(SPEAKER_PIN, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(SPEAKER_PIN, LOW);
-    delayMicroseconds(tone);
-  }
-}
+
---
+++ Buzzer/Sound.h
@@ -0,0 +1,10 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+
+struct Sound {
+  int note;
+  int beat;
+};
+
+#endif
